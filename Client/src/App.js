cd3ba57d040196e78b02525e37271bfafdf61b29
ravishrawal <rr2914@columbia.edu>
2020-11-28 15:34:27 -0500

    first commit

---
+++ Client/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import React, {Component} from 'react'
+import {Route, BrowserRouter} from 'react-router-dom'
+
+
+class App extends Component {
+  
+  componentDidMount() {
+      console.log('Mounted!')
+  }
+  render(){
+    console.log(this.props)
+    return (
+      <div className="App" >
+        <header className="App-header">
+        </header>
+        // <BrowserRouter>
+        //   <Route path="/passcode" component={OTPInputForm} />
+        //   <Route path="/visitors/new" component={NewVisitorForm} />
+        // </BrowserRouter>
+      </div>
+    );
+  }
+}
+
+
+export default App;
